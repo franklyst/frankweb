e743d01cc7c93013cadc4a48aa21ab463e06ae2c
stepanzak <stepanzak@users.noreply.github.com>
2026-07-21 18:14:53 +0000

    deploy: 14330778f158c3a21427d1533edc4bdbd594996c

--- kiosk.js
+++ kiosk.js
@@ -2,6 +2,7 @@ let copyright = document.getElementById("pagefooter__copyright")
 let instalink = document.getElementById("instagram-link")
 copyright.dataset.clickCount = 0
 copyright.dataset.lastClick = 0
+let t; // idle timer
 
 copyright.addEventListener("click", e => {
   e.preventDefault()
@@ -46,6 +47,7 @@ function activateKiosk() {
       link.removeEventListener('click', openLinkModal)
     });
     document.body.oncontextmenu = null
+    clearTimeout(t)
   } else {
     // enable kiosk
     document.documentElement.requestFullscreen()
@@ -54,6 +56,7 @@ function activateKiosk() {
       link.addEventListener('click', openLinkModal)
     });
     document.body.oncontextmenu = _ => false
+    noIdlingHere();
   }
 }
 
@@ -62,3 +65,34 @@ function openLinkModal(e) {
   const target = document.getElementById(this.dataset.kioskopen)
   target.showModal()
 }
+
+
+// Source - https://stackoverflow.com/a/24989958
+// Posted by Frank Conijn - Support Ukraine, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-07-21, License - CC BY-SA 4.0
+
+function noIdlingHere() {
+
+  function yourFunction() {
+    const kioskMode = sessionStorage.getItem("kioskModeEnabled")
+    if (kioskMode && kioskMode == "true") {
+      window.location.replace(document.body.dataset.kioskurl)
+    }
+  }
+
+  function resetTimer() {
+    clearTimeout(t); // global function
+    t = setTimeout(yourFunction, 120000);  // time is in milliseconds (2 min)
+  }
+
+  window.addEventListener('load', resetTimer, true);
+  window.addEventListener('mousemove', resetTimer, true);
+  window.addEventListener('mousedown', resetTimer, true);
+  window.addEventListener('touchstart', resetTimer, true);
+  window.addEventListener('touchmove', resetTimer, true);
+  window.addEventListener('click', resetTimer, true);
+  window.addEventListener('keydown', resetTimer, true);
+  window.addEventListener('scroll', resetTimer, true);
+  window.addEventListener('wheel', resetTimer, true);
+}
+
